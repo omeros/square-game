aacd9005066508c3080df3af7c919f04f735907f
omeros <omer.golan.dev@gmail.com>
2024-11-15 18:41:39 +0200

    "Update img style in bound-app.vue: change width from 50% to 100%"

--- src/views/bound-app.vue
+++ src/views/bound-app.vue
@@ -523,7 +523,7 @@ img{
   position: absolute;  
   top: 0;  
   left: 0;  
-  width: 50%;  
+  width: 100%;  
   height: 100%;  
   background-image: linear-gradient(to right, transparent, rgba(255, 255, 255, 0.8), transparent);  
   animation: shine 2s ease-in-out infinite;  
